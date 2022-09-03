f41d7107ae172a9edd86bb9421cdb0ddf817f716
Sachin Bhat Gopalakrishna <52129700+sachinbhat2001@users.noreply.github.com>
2022-09-03 14:59:18 +0530

    Update ripplecarryadder.v
    Alternate code with fewer lines

--- RippleCarryAdder/ripplecarryadder.v
+++ RippleCarryAdder/ripplecarryadder.v
@@ -21,3 +21,30 @@ module ha(output cout,sum,
 		input a,b);
 assign {cout,sum}=a+b;
 endmodule
+
+/*
+
+// ALTERNATE CODE WITH FEWER LINES
+
+module RippleCarryAdder(output cout,
+			output [3:0]sum,
+	  		input [3:0]a,b,
+			input cin);
+
+wire c1,c2,c3;
+FA f1(c1,sum[0],a[0],b[0],cin);
+FA f2(c2,sum[1],a[1],b[1],c1);
+FA f3(c3,sum[2],a[2],b[2],c2);
+FA f4(cout,sum[3],a[3],b[3],c3);
+endmodule
+
+
+//Full Adder Module 
+module FA(output cout,sum,
+	  input a,b,cin);
+
+assign {cout,sum}=a+b+cin; //Behavioural Modelling
+endmodule
+
+
+*/
